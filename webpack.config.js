75f370a14a77c521e2d487bafb5183cc2c4579dd
coucou_miya <bingbing.yan@efrei.net>
2016-12-29 19:10:19 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+
+const parts = require('./webpack.parts');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  style: [
+    path.join(__dirname, 'node_modules', 'purecss'),
+    path.join(__dirname, 'app', 'main.css')
+  ],
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    style: PATHS.style,
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
+
+module.exports = function(env) {
+  if (env === 'build') {
+    return merge(
+      common,
+      {
+        devtool: 'source-map',
+        output: {
+          path: PATHS.build,
+          filename: '[name].[chunkhash].js',
+          // This is used for code splitting. The setup
+          // will work without but this is useful to set.
+          chunkFilename: '[chunkhash].js',
+          // Tweak this to match your GitHub project name
+          publicPath: '/webpack-demo/'
+        }
+      },
+      parts.clean(PATHS.build),
+      parts.setFreeVariable(
+        'process.env.NODE_ENV',
+        'production'
+      ),
+      parts.extractBundle({
+        name: 'vendor',
+        entries: ['react']
+      }),
+        parts.extractCSS(PATHS.style),
+        parts.purifyCSS([PATHS.app]),
+      parts.minify()
+
+    );
+  }
+
+  return merge(
+    common,
+    {
+      devtool: 'eval-source-map',
+      // Disable performance hints during development
+      performance: {
+        hints: false
+      }
+    },
+    parts.setupCSS(PATHS.style),
+    parts.devServer({
+      // Customize host/port here if needed
+      host: process.env.HOST,
+      port: process.env.PORT
+    })
+  );
+};
